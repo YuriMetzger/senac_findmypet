3f1f5bf0d0ab89673a8b63ae391520ffdc5b5232
YuriMetzger <31169797+YuriMetzger@users.noreply.github.com>
2018-06-17 01:34:20 -0300

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require_once("src/dao/DBConnection.php");
+require_once("src/utils/AuthUtils.php");
+?> 
+<div class="container">
+  <h3>Right Aligned Navbar</h3>
+  <p>The .navbar-right class is used to right-align navigation bar buttons.</p>
+</div>
+
+</body>
+</html>
