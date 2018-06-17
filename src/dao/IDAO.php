eb2dd1ef394184959190fea23491ef35ca3befb4
YuriMetzger <31169797+YuriMetzger@users.noreply.github.com>
2018-06-17 01:31:12 -0300

    Update IDAO.php

--- src/dao/IDAO.php
+++ src/dao/IDAO.php
@@ -1,6 +1,27 @@
-public interface IDAO {
+<?php
+ require_once 'DBConnection.php';
 
-	public function insert($object, $conn);
-	public function delete($id, $conn);
-	public function select($id, $conn);
+abstract class IDAO extends DBConnection {
+	
+	protected $table;
+
+	abstract public function insert();
+	#abstract public function update($ID);
+	
+	#public function delete($id, $conn);
+	public function find($id){
+		$sql  = "SELECT * FROM $this->table WHERE id = :id";
+		$stmt = DBConnection::prepare($sql);
+		$stmt->bindParam(':id', $id); 
+		$stmt->execute();
+		return $stmt-fetch();
+	}
+	public function findAll(){
+		$sql  = "SELECT * FROM $this->table;";
+		$stmt = DBConnection::prepare($sql);
+		$stmt->execute();
+		return $stmt->fetchAll();
+	}
+
 }
+?>
